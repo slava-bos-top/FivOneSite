04f3b5b476a2bed3ee2175e91eee842e83e96223
Slava Ribachik <kot2321mail@gmail.com>
2025-07-18 22:47:29 +0300

    log

--- src/components/TelegramLogin.jsx
+++ src/components/TelegramLogin.jsx
@@ -28,7 +28,8 @@ const TelegramLogin = () => {
         const checkData = await checkRes.json();
         clearInterval(intervalId);
         setChecking(false);
-        alert("✅ Реєстрація підтверджена!");
+        alert("✅ Реєстрація підтверджена!", checkData);
+        console.log(checkData)
         localStorage.setItem("user", JSON.stringify({
           name: checkData.name,
           surname: checkData.surname,
