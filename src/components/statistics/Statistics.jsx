fd4e64275568dea2d1ad9397d54cc5efbb4cbe14
Slava Ribachik <kot2321mail@gmail.com>
2025-07-20 00:14:51 +0300

    update

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -5,46 +5,92 @@ import "./Statistics.css"
 
 // Твої дані марафонів
 // Дані
-const marathonsData = [
-    {
-      title: "Марафон 1",
-      weeks: [
-        { lessons: [{ completed: true }, { completed: true }, { completed: false }] },
-        { lessons: [{ completed: true }, { completed: false }, { completed: false }] }
-      ]
-    },
-    {
-      title: "Марафон 2",
-      weeks: [
-        { lessons: [{ completed: true }, { completed: true }] },
-        { lessons: [{ completed: true }, { completed: true }] }
-      ]
-    },
-    {
-        title: "Марафон 3",
-        weeks: [
-          { lessons: [{ completed: true }, { completed: true }, { completed: false }] },
-          { lessons: [{ completed: true }, { completed: false }, { completed: false }] }
-        ]
-    },
-    {
-        title: "Марафон 4",
-        weeks: [
-          { lessons: [{ completed: true }, { completed: true }] },
-          { lessons: [{ completed: true }, { completed: true }] }
-        ]
-    },
-    {
-        title: "Марафон 5",
-        weeks: [
-          { lessons: [{ completed: true }, { completed: true }] },
-          { lessons: [{ completed: true }, { completed: true }] }
-        ]
-    },
-    // ... ще 3 марафони
-  ];
 
 function CircularProgress({ percentage, colors }) {
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [image, setImage] = useState("")
+    const [maraphone1, setMaraphone1] = useState("")
+    const [maraphone2, setMaraphone2] = useState("")
+    const [maraphone3, setMaraphone3] = useState("")
+    const [maraphone4, setMaraphone4] = useState("")
+    const [maraphone5, setMaraphone5] = useState("")
+    const [week1, setWeek1] = useState("")
+    const [week2, setWeek2] = useState("")
+    const [week3, setWeek3] = useState("")
+    const [week4, setWeek4] = useState("")
+    const [week5, setWeek5] = useState("")
+    const [week6, setWeek6] = useState("")
+    const [week7, setWeek7] = useState("")
+    const [week8, setWeek8] = useState("")
+    const [week9, setWeek9] = useState("")
+    const [week10, setWeek10] = useState("")
+    const [week11, setWeek11] = useState("")
+    const [week12, setWeek12] = useState("")
+    const [week13, setWeek13] = useState("")
+  
+    useEffect(() => {
+      const savedUser = localStorage.getItem("user");
+      if (savedUser) {
+        const { name, surname, photo, isLoggedIn, maraphone1, maraphone2, maraphone3, maraphone4, maraphone5, week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13 } = JSON.parse(savedUser);
+        if (isLoggedIn) {
+          setSignIn(true)
+          setName(name)
+          setSurname(surname)
+          setImage(photo)
+          setMaraphone1(maraphone1)
+          setMaraphone2(maraphone2)
+          setMaraphone3(maraphone3)
+          setMaraphone4(maraphone4)
+          setMaraphone5(maraphone5)
+          setWeek1(week1)
+          setWeek2(week2)
+          setWeek3(week3)
+          setWeek4(week4)
+          setWeek5(week5)
+          setWeek6(week6)
+          setWeek7(week7)
+          setWeek8(week8)
+          setWeek9(week9)
+          setWeek10(week10)
+          setWeek11(week11)
+          setWeek12(week12)
+          setWeek13(week13)
+          // Наприклад, показати імʼя в Header
+        }
+      }
+
+        const TOTAL_WEEKS = 2;
+        const TOTAL_TASKS_PER_WEEK = 7;
+
+        const marathons = Array.from({ length: 5 }, (_, index) => {
+            const marathonId = index + 1;
+            const weekStart = marathonId * 2 - 1;
+
+            const weeks = [0, 1].map(i => {
+                const weekKey = `week${weekStart + i}`;
+                const completedTasks = userData?.[weekKey] || 0;
+                return {
+                week: weekStart + i,
+                completedTasks,
+                totalTasks: TOTAL_TASKS_PER_WEEK
+                };
+            });
+
+            return {
+                id: marathonId,
+                completedWeeks: userData?.[`maraphone${marathonId}`] || 0,
+                totalWeeks: TOTAL_WEEKS,
+                weeks
+            };
+        });
+        console.log(marathons)
+    }, []);
+
+
+
+
+
   const strokeDasharray = 283;
   const strokeDashoffset = strokeDasharray * (1 - percentage / 100);
 
@@ -90,7 +136,7 @@ function SplitScreen() {
   return (
     <div style={{
       display: "flex",
-      height: "100vh",
+      height: "80vh",
       fontFamily: "sans-serif",
       backgroundColor: "#FFCE07",
       padding: "20px",
