a0a8fb1439939d036000698670587894b6b638e6
Slava Ribachik <kot2321mail@gmail.com>
2025-07-20 19:35:02 +0300

    update

--- src/components/mainSignIn/MainSignIn.jsx
+++ src/components/mainSignIn/MainSignIn.jsx
@@ -4,12 +4,33 @@ import Statistics from '../statistics/Statistics';
 import HeaderSignIn from '../headerSignIn/HeaderSignIn';
 
 export default function Main() {
+    const [signIn, setSignIn] = useState(false)
+
+    useEffect(() => {
+
+        const savedUser = localStorage.getItem("user");
+        if (savedUser) {
+          const { isLoggedIn } = JSON.parse(savedUser);
+          if (isLoggedIn) {
+            setSignIn(true)
+            // Наприклад, показати імʼя в Header
+          }
+        }
+      }, []);
 
   return (
     <div className='main'>
-        <HeaderSignIn />
-        <Statistics />
-        <Footer />
+        {signIn ? (
+            <>
+                <HeaderSignIn />
+                <Statistics />
+                <Footer />
+            </>
+        ) : (
+            <>
+                <Link to="/login" className="header_nav_button">Зареєструватись</Link>
+            </>
+        )}
     </div>
   )
 }
