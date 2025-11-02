0432d42d597284d4eac68323df9e8c43e1966fef
Slava Ribachik <kot2321mail@gmail.com>
2025-11-02 16:16:44 +0300

    update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -7,6 +7,11 @@ import course1 from "./../../img/course1.jpg"
 import course2 from "./../../img/course2.png"
 import course3 from "./../../img/course3.png"
 
+import marathon1 from "./../../img/marathon1.jpg"
+import marathon2 from "./../../img/marathon2.jpg"
+import marathon3 from "./../../img/marathon3.jpg"
+import marathon4 from "./../../img/marathon4.jpg"
+
 import soundGif from './images/sound.gif';
 import audio from "./media/ambient.mp3";
 
@@ -50,6 +55,57 @@ const coursesDataBlock = [
   },
 ];
 
+const marathonsDataBlock = [
+  {
+    title: 'Марафон з фізики',
+    description: 'Відкрийте закони Всесвіту через досліди, рух і енергію навколо нас.',
+    details: [
+      '«Марафон з фізики» – це практичні експерименти, відеоуроки та задачі, які допоможуть зрозуміти фізику не з підручника,',
+      'а з реального життя.'
+    ],
+    image: marathon1,
+    logo: logo,
+    color: '#16BAAE',
+    number: 0
+  },
+  {
+    title: 'Марафон з хімії',
+    description: 'Закохайтесь у хімію через експерименти та цікаві завдання.',
+    details: [
+      '«Х Марафон» – це щоденні експерименти та відеолекції зі знаннями, які знадобляться кожному в житті.'
+    ],
+    image: marathon2,
+    logo: logo,
+    color: '#94BF47',
+    number: 1
+  },
+  {
+    title: 'Марафон з креативності',
+    description: 'Станьте креативнішими та навчіться нестандартно вирішувати ситуації.',
+    details: [
+      '«Марафон з креативності» – це тритижневий мікрокурс, що складається з вправ та завдань',
+      'зі сфер дизайну, культури та мистецтва.'
+    ],
+    image: marathon3,
+    logo: logo,
+    color: '#FFCE07',
+    number: 2
+  },
+  {
+    title: 'Марафон з програмування',
+    description: 'Опануйте базові навички ІТ за допомогою щоденних мінілекцій та завдань.',
+    details: [
+      '«Програмування для всіх» – це мікрокурс з основ трьох мов програмування для тих,',
+      'хто боїться почати програмувати.'
+    ],
+    image: marathon4,
+    logo: logo,
+    color: '#E95C28',
+    number: 2
+  },
+];
+
+
 export default function Gallery() {
   const { hash } = useLocation();
 
@@ -79,6 +135,30 @@ export default function Gallery() {
 
           <div className="courses__grid-gallery">
             {coursesDataBlock.map((course, i) => (
+              <div key={i} className="course-card-gallery" style={{ '--accent': getColor(i) }}>
+                <img src={course.image} alt="course_img" className="course-card__image-gallery" />
+                <div className="course-card__content-gallery">
+                  <img src={logo} alt="logo" className="course-card__logo-gallery" />
+                  <h3 style={{color: course.color}}>{course.title}</h3>
+                  <div className="course-card__description-gallery">
+                    <p className="course-card__description_one-gallery">{course.description}</p>
+                    <p className="course-card__description_two-gallery">{course.details}</p>
+                  </div>
+                  <Link to="/course" state={coursesData[course.number]} className="course-card__button-gallery" style={{background: course.color}}>
+                    Дізнатися більше
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="courses-gallery">
+          <div className="courses__header-gallery">
+            <h2>Наші курси</h2>
+          </div>
+
+          <div className="courses__grid-gallery">
+            {marathonsDataBlock.map((course, i) => (
               <div key={i} className="course-card-gallery" style={{ '--accent': getColor(i) }}>
                 <img src={course.image} alt="course_img" className="course-card__image-gallery" />
                 <div className="course-card__content-gallery">
